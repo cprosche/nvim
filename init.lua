54cb83f8d165e37a34ecbc33b47204423a3b0373
“Cade <caderosche@gmail.com>
2023-12-20 19:58:03 -0700

    added gruvbox theme

--- init.lua
+++ init.lua
@@ -9,3 +9,26 @@ opt.autoindent = true
 opt.wrap = false
 opt.background = "dark"
 opt.termguicolors = true
+vim.g.mapleader = ";"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+opt.rtp:prepend(lazypath)
+
+local plugins = {
+{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+}
+
+require("lazy").setup(plugins)
+
+vim.cmd([[colorscheme gruvbox]])
