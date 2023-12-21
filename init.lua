ba73c6ed7d00a4c2e1ae6ba65f5a1ce9cb1bd776
“Cade <caderosche@gmail.com>
2023-12-20 19:37:01 -0700

    added init.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+opt.number = true
+opt.fillchars = { eob = " " } 
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.wrap = false
+opt.background = "dark"
+opt.termguicolors = true
