17936fe81aeb44a7e8b50c80ec633ea53abbc700
“Cade <caderosche@gmail.com>
2024-04-27 13:47:26 -0600

    added comment

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ vim.g.mapleader = ";"
 opt.spell = true
 opt.spelllang = "en_us"
 
+-- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
